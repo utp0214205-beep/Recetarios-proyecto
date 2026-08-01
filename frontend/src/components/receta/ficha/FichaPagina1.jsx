cc464503d283a58f93a7d6fa7336c301dd44b218
utp0214205-beep <utp0214205@alumno.utpuebla.edu.mx>
2026-08-01 17:22:09 +0000

    Mayor vista

--- frontend/src/components/receta/ficha/FichaPagina1.jsx
+++ frontend/src/components/receta/ficha/FichaPagina1.jsx
@@ -1,5 +1,7 @@
 import encabezadoFicha from "../../../assets/images/encabezado-ficha.png";
+
 function FichaPagina1({ receta }) {
+
     const ingredientes = receta.ingredientes || [];
 
     const FILAS = 17;
@@ -23,234 +25,288 @@ function FichaPagina1({ receta }) {
 
             </header>
 
+            <div className="contenido-ficha">
 
-            <table className="tabla-datos">
+                <table className="tabla-datos">
 
-                <tbody>
+                    <tbody>
 
-                    <tr>
-                        <th>Asignatura</th>
-                        <td>{receta.asignatura}</td>
+                        <tr>
 
-                        <th>Clasificación</th>
-                        <td>{receta.clasificacion}</td>
-                    </tr>
+                            <th>Asignatura</th>
 
-                    <tr>
-                        <th>Nombre del platillo</th>
-                        <td>{receta.nombre_platillo}</td>
+                            <td>{receta.asignatura}</td>
 
-                        <th>Número de práctica</th>
-                        <td>{receta.numero_practica}</td>
-                    </tr>
+                            <th>Clasificación</th>
 
-                    <tr>
-                        <th>Fecha</th>
-                        <td>{
-                            receta.fecha
-                                ? new Date(receta.fecha).toLocaleDateString("es-MX")
-                                : ""
-                        }</td>
-
-                        <th>Tiempo preparación</th>
-                        <td>{receta.tiempo_preparacion}</td>
-                    </tr>
-
-                    <tr>
-                        <th>Total producción</th>
-                        <td>{receta.total_produccion}</td>
-
-                        <th>Aporte nutrimental</th>
-                        <td>{receta.aporte_nutrimental}</td>
-                    </tr>
-
-                    <tr>
-                        <th>Porciones</th>
-                        <td>{receta.numero_porciones}</td>
-
-                        <th>Conservación</th>
-                        <td>{receta.metodo_tiempo_conservacion}</td>
-                    </tr>
-
-                    <tr>
-                        <th>Cantidad por porción</th>
-                        <td>{receta.cantidad_porcion}</td>
-
-                        <th>Maridaje</th>
-                        <td>{receta.maridaje}</td>
-                    </tr>
-
-                </tbody>
-
-            </table>
-
-
-            <div className="contenedor-ingredientes">
-
-                <table className="tabla-ingredientes">
-
-                <thead>
-
-                    <tr>
-
-                        <th>CANT.</th>
-
-                        <th>UNIDAD</th>
-
-                        <th>MATERIA PRIMA</th>
-
-                        <th>COSTO UNITARIO</th>
-
-                        <th>% DE RENDIMIENTO</th>
-
-                        <th>IMPORTE</th>
-
-                    </tr>
-
-                </thead>
-
-                <tbody>
-
-                    {ingredientes.map((item) => (
-
-                        <tr key={item.id_ingrediente_receta}>
-
-                            <td>{item.cantidad}</td>
-
-                            <td>{item.unidad}</td>
-
-                            <td>{item.nombre}</td>
-
-                            <td>
-
-                                {item.costo_unitario
-                                    ? `$ ${Number(item.costo_unitario).toFixed(2)}`
-                                    : ""}
-
-                            </td>
-
-                            <td>
-
-                                {item.rendimiento
-                                    ? `${item.rendimiento}%`
-                                    : ""}
-
-                            </td>
-
-                            <td>
-
-                                {item.importe
-                                    ? `$ ${Number(item.importe).toFixed(2)}`
-                                    : ""}
-
-                            </td>
+                            <td>{receta.clasificacion}</td>
 
                         </tr>
 
-                    ))}
+                        <tr>
 
-                    {Array.from({ length: filasVacias }).map((_, i) => (
+                            <th>Nombre del platillo</th>
 
-                        <tr key={`vacia-${i}`}>
+                            <td>{receta.nombre_platillo}</td>
 
-                            <td>&nbsp;</td>
+                            <th>Número de práctica</th>
 
-                            <td></td>
-
-                            <td></td>
-
-                            <td></td>
-
-                            <td></td>
-
-                            <td></td>
+                            <td>{receta.numero_practica}</td>
 
                         </tr>
 
-                    ))}
+                        <tr>
 
-                </tbody>
+                            <th>Fecha</th>
+
+                            <td>
+                                {
+                                    receta.fecha
+                                        ? new Date(receta.fecha).toLocaleDateString("es-MX")
+                                        : ""
+                                }
+                            </td>
+
+                            <th>Tiempo preparación</th>
+
+                            <td>{receta.tiempo_preparacion}</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <th>Total producción</th>
+
+                            <td>{receta.total_produccion}</td>
+
+                            <th>Aporte nutrimental</th>
+
+                            <td>{receta.aporte_nutrimental}</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <th>Porciones</th>
+
+                            <td>{receta.numero_porciones}</td>
+
+                            <th>Conservación</th>
+
+                            <td>{receta.metodo_tiempo_conservacion}</td>
+
+                        </tr>
+
+                        <tr>
+
+                            <th>Cantidad por porción</th>
+
+                            <td>{receta.cantidad_porcion}</td>
+
+                            <th>Maridaje</th>
+
+                            <td>{receta.maridaje}</td>
+
+                        </tr>
+
+                    </tbody>
 
                 </table>
 
-            </div>
+                <div className="contenedor-ingredientes">
 
+                    <table className="tabla-ingredientes">
 
-            <div className="pie-pagina1">
+                        <thead>
 
-                <div>
+                            <tr>
 
-                    <strong>
+                                <th>CANT.</th>
 
-                        Buenas Prácticas de Higiene:
+                                <th>UNIDAD</th>
 
-                    </strong>
+                                <th>MATERIA PRIMA</th>
 
-                    <p>
+                                <th>COSTO UNITARIO</th>
 
-                        {receta.informacion_complementaria?.buenas_practicas}
+                                <th>% DE RENDIMIENTO</th>
 
-                    </p>
+                                <th>IMPORTE</th>
 
-                </div>
+                            </tr>
 
-                <div>
-
-                    <table className="tabla-costos">
+                        </thead>
 
                         <tbody>
 
-                            <tr>
+                            {
 
-                                <th>
+                                ingredientes.map((item) => (
 
-                                    Costo de insumos de la receta:
+                                    <tr key={item.id_ingrediente_receta}>
 
-                                </th>
+                                        <td>{item.cantidad}</td>
 
-                                <td>
+                                        <td>{item.unidad}</td>
 
-                                    $
+                                        <td>{item.nombre}</td>
 
-                                    {
+                                        <td>
 
-                                        Number(
-                                            receta.costo_total || 0
-                                        ).toFixed(2)
+                                            {
 
-                                    }
+                                                item.costo_unitario
+                                                    ? `$ ${Number(item.costo_unitario).toFixed(2)}`
+                                                    : ""
 
-                                </td>
+                                            }
 
-                            </tr>
+                                        </td>
 
-                            <tr>
+                                        <td>
 
-                                <th>
+                                            {
 
-                                    Costo de insumos por porción:
+                                                item.rendimiento
+                                                    ? `${item.rendimiento}%`
+                                                    : ""
 
-                                </th>
+                                            }
 
-                                <td>
+                                        </td>
 
-                                    $
+                                        <td>
 
-                                    {
+                                            {
 
-                                        Number(
-                                            receta.costo_por_porcion || 0
-                                        ).toFixed(2)
+                                                item.importe
+                                                    ? `$ ${Number(item.importe).toFixed(2)}`
+                                                    : ""
 
-                                    }
+                                            }
 
-                                </td>
+                                        </td>
 
-                            </tr>
+                                    </tr>
+
+                                ))
+
+                            }
+
+                            {
+
+                                Array.from({
+                                    length: filasVacias
+                                }).map((_, i) => (
+
+                                    <tr key={`vacia-${i}`}>
+
+                                        <td>&nbsp;</td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                    </tr>
+
+                                ))
+
+                            }
 
                         </tbody>
 
                     </table>
+
+                </div>
+
+                <div className="pie-pagina1">
+
+                    <div>
+
+                        <strong>
+
+                            Buenas Prácticas de Higiene
+
+                        </strong>
+
+                        <p>
+
+                            {
+
+                                receta.informacion_complementaria
+                                    ?.buenas_practicas
+
+                            }
+
+                        </p>
+
+                    </div>
+
+                    <div>
+
+                        <table className="tabla-costos">
+
+                            <tbody>
+
+                                <tr>
+
+                                    <th>
+
+                                        Costo de insumos de la receta
+
+                                    </th>
+
+                                    <td>
+
+                                        $
+
+                                        {
+
+                                            Number(
+                                                receta.costo_total || 0
+                                            ).toFixed(2)
+
+                                        }
+
+                                    </td>
+
+                                </tr>
+
+                                <tr>
+
+                                    <th>
+
+                                        Costo de insumos por porción
+
+                                    </th>
+
+                                    <td>
+
+                                        $
+
+                                        {
+
+                                            Number(
+                                                receta.costo_por_porcion || 0
+                                            ).toFixed(2)
+
+                                        }
+
+                                    </td>
+
+                                </tr>
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
 
                 </div>
 
